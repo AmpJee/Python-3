d647b76293faa2aaa040ecf00c5249bbcb9689ce
AmpJee <amp.2549@gmail.com>
2025-01-13 11:22:14 +0700

    init circle

--- project-day1.py
+++ project-day1.py
@@ -1,3 +1,22 @@
-length = int(input())
-width = int(input())
-print(length * width)
+class Circle:
+    def __init__(self, radius, x, y):
+        self.radius = radius
+        self.x = x
+        self.y = y
+
+
+    def draw(self):
+        print(f"{self.x}, {self.y}")
+ 
+
+    def move(self):
+        self.x += 1
+        self.y += 1
+
+
+if __name__ == "__main__":
+    c = Circle(4, 5, 6)
+    for i in range(4):
+        c.move()
+        c.draw()
+        
