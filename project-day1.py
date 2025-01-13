0e0a9b3c001a9b6d42d8fe6fb7ce2f7a0d606868
AmpJee <amp.2549@gmail.com>
2025-01-13 10:00:07 +0700

    no text

--- project-day1.py
+++ project-day1.py
@@ -1,4 +1,3 @@
-length = input("Enter the length of the rectangle: ")
-width = input("Enter the width of the rectangle: ")
-area = int(length) * int(width)
-print("The area of the rectangle is", area)
+length = int(input())
+width = int(input())
+print(length * width)
