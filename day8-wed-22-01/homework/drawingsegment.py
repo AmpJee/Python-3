533882a18d50039b1bf5aff1283a183c701de85d
AmpJee <83893165+AmpJee@users.noreply.github.com>
2025-01-23 20:39:23 +0700

    add time

--- day8-wed-22-01/homework/drawingsegment.py
+++ day8-wed-22-01/homework/drawingsegment.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rd
+import time
 
-segments = np.array([[rd.randint(0, 10), rd.randint(0, 10), rd.randint(0, 10), rd.randint(0, 10)] for i in range(10)])
-regtangle = [-10, -10, 10, 10]
+segments = np.array([[rd.randint(-10, 10), rd.randint(-10, 10), rd.randint(-10, 10), rd.randint(-10, 10)] for _ in range(500)])
+regtangle = [-6, -6, 6, 6]
 
 
-def isregtangle(regtangle, segment):
+def filter_segment(regtangle, segment):
     x1, y1, x2, y2 = segment
     if (min(regtangle[0], regtangle[2]) <= x1 <= max(regtangle[0], regtangle[2]) 
         and min(regtangle[0], regtangle[2]) <= x2 <= max(regtangle[0], regtangle[2])
@@ -15,10 +16,28 @@ def isregtangle(regtangle, segment):
             return True
     return False
 
-for segment in segments:
-    x1, y1, x2, y2 = segment
-    if isregtangle(regtangle, segment) == True:
-        plt.plot([x1, x2], [y1, y2])
+def graph_plot(segments):
+    fig, (ax, bx) = plt.subplots(1, 2, figsize=(10, 5))
+    ax.set_xlim(-10, 10)
+    ax.set_ylim(-10, 10)
+    bx.set_xlim(-10, 10)
+    bx.set_ylim(-10, 10)
 
+    start = time.time()
 
-plt.show()
+    for segment in segments:
+        x1, y1, x2, y2 = segment
+        ax.plot([x1, x2], [y1, y2], color=np.random.rand(3))
+
+    for segment in segments:
+        x1, y1, x2, y2 = segment
+        if filter_segment(regtangle, segment) == True:
+            bx.plot([x1, x2], [y1, y2], color=np.random.rand(3))
+
+    stop = time.time()
+
+    plt.show()
+
+    return stop - start
+
+print(f"Time: {graph_plot(segments)}")
