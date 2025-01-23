5dd227e35a744b7c203d9aac941e0aa6841b134d
AmpJee <83893165+AmpJee@users.noreply.github.com>
2025-01-23 10:48:01 +0700

    step2-scatter plot

--- day9-thu-23-01/fromcsv.py
+++ day9-thu-23-01/fromcsv.py
@@ -1,8 +1,9 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
 points = np.genfromtxt('points.csv', delimiter=',')
 distances = np.genfromtxt('distances.csv', delimiter=',')
 
-
+plt.scatter(points[:, 0], points[:, 1], c=distances)
+plt.show()
