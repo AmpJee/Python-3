868c220e0aecc8e65088e74b963aef56d9a5ae5e
AmpJee <83893165+AmpJee@users.noreply.github.com>
2025-01-23 23:14:15 +0700

    update function

--- day9-thu-23-01/homework/k-means_clusters.py
+++ day9-thu-23-01/homework/k-means_clusters.py
@@ -1,20 +1,35 @@
 import matplotlib.pyplot as plt
 import random as rd
+import math
 
-centriods = [[5, 6], [-3, 5], [1, 9]]
 
-points = []
-for cx, cy in centriods:
-    for _ in range(500):
-        x = rd.gauss(cx, 0.5)
-        y = rd.gauss(cy, 0.5)
-        points.append([x, y])
+def generate_points(centroids):
+    points = []
+    for cx, cy in centroids:
+        for _ in range(500):
+            x = rd.gauss(cx, 0.5)
+            y = rd.gauss(cy, 0.5)
+            points.append([x, y])
+    return points
 
-plt.figure(figsize=(10, 10))
-plt.scatter([x for x, y in points], [y for x, y in points], s=4, label='Points')
-plt.scatter([x for x, y in centriods], [y for x, y in centriods], s=10, color='black', label='Centroids')
 
-plt.legend()
-plt.title('K-Means Clustering')
-plt.savefig('k-means_clusters.png')
-plt.show()
+def plot_initial(points, centroids, ):
+    plt.figure()
+    plt.scatter([x for x, y in points], [y for x, y in points], s=4, label='Points')
+    plt.scatter([x for x, y in centroids], [y for x, y in centroids], s=10, color='black', label='Centroids')
+    plt.legend()
+    plt.title('Initial Points and Centroids')
+    plt.savefig('k-means initial.png')
+    plt.show()
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+ 
+def main():
+    centroids = [(-5, 3), (4, -7), (1, 9), (10, -1)]
+    points = generate_points(centroids)
+    plot_initial(points, centroids)
+
+
+if __name__ == '__main__':
+    main()
