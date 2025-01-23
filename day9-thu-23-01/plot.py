b6ccfcdf9c80144c41eb5315acd4e713eb87363b
AmpJee <83893165+AmpJee@users.noreply.github.com>
2025-01-23 09:49:46 +0700

    step1-plot graph

---
+++ day9-thu-23-01/plot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def f1(x):
+    return x**2
+
+def f2(x):
+    return x * np.sin(2*x)
+
+
+def f3(x):
+    return np.arctan(x)
+
+
+x = np.linspace(-2, 2, 100)
+y1 = f1(x)
+y2 = f2(x)
+y3 = f3(x)
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+
+plt.show()
