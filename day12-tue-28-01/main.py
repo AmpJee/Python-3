f043752a2ebb96389b125500e3fec3071c606c88
AmpJee <83893165+AmpJee@users.noreply.github.com>
2025-01-28 11:49:09 +0700

    calculator

--- day12-tue-28-01/main.py
+++ day12-tue-28-01/main.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("main.html")
 
 
-@app.route("/test/<int:value1>/<int:value2>")
-def test_square(value1, value2):
-    return f"<p>Answer is {value1**2 + value2**2}</p>"
+@app.route("/calculate")
+def show_calc_form():
+    return render_template("calculator.html")
+
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    number = int(request.form["number"])
+    result = number**2
+    return render_template('calculator.html', result=result)
